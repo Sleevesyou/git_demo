6d47412bbdf1c8b6a36e852765a7765097a3fdb9
dengyi <2772630136@qq.com>
2022-01-09 10:19:55 +0800

    master 000

--- src/main/java/com/example/demo/Test01.java
+++ src/main/java/com/example/demo/Test01.java
@@ -4,5 +4,7 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("Git测试！！！");
         System.out.println("当前是dev分支");
+        System.out.println("当前是master分支-----01");
+        System.out.println("当前是master分支-----02");
     }
 }
